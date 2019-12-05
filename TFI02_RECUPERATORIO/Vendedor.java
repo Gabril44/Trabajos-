29b835f7610e05be6ef52457e909cf300b037d77
Gabriel Acevedo <53916521+Gabril44@users.noreply.github.com>
2019-12-05 12:04:55 -0300

    Add files via upload

--- TFI02_RECUPERATORIO/Vendedor.java
+++ TFI02_RECUPERATORIO/Vendedor.java
@@ -6,6 +6,38 @@ public class Vendedor implements ParaPersona{
 	private int cantidadDeVentas;
 	private int dni;
 	
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + cantidadDeVentas;
+		result = prime * result + dni;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vendedor other = (Vendedor) obj;
+		if (cantidadDeVentas != other.cantidadDeVentas)
+			return false;
+		if (dni != other.dni)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
 
 	@Override
 	public String toString() {
